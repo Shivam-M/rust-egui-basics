c4b6bd42d1291b6aa3a1a5c62323b4eef1807730
Shivam-M <shivammann0@gmail.com>
2025-11-13 21:11:09 +0000

    add structs for storing objects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+use std::cell::RefCell;
 use eframe::egui;
 
 static WINDOW_WIDTH: f32 = 1280.0;
@@ -13,11 +15,42 @@ fn main() -> eframe::Result<()> {
     return eframe::run_native(
         "GUI Input Test - moving an object around with basic collision detection",
         options,
-        Box::new(|_cc| Ok(Box::new(CollisionApp {})))
+        Box::new(|_cc| Ok(Box::new(CollisionApp::new())))
     );
 }
 
-struct CollisionApp {}
+struct Object {
+    width: f32,
+    height: f32,
+    position: egui::Pos2,
+    velocity: egui::Vec2,
+}
+
+impl Object {
+    fn new(width: f32, height: f32, position: egui::Pos2, velocity: egui::Vec2) -> Self {
+        Self { width, height, position, velocity }
+    }
+}
+
+struct CollisionApp {
+    controlled_object_index: usize,  // alternative is storing the object using refcells which look complicated and messier
+    objects: Vec<Object>,
+}
+
+impl CollisionApp {
+    fn new() -> Self {
+        let controlled_object = Object::new(
+            50.0,
+            50.0,
+            egui::Pos2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0),
+            egui::Vec2::new(0.0, 0.0),
+        );
+
+        let objects = vec![controlled_object];
+
+        Self { controlled_object_index: 0, objects }
+    }
+}
 
 impl eframe::App for CollisionApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
