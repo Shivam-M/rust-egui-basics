61ddaf7c5a6ff850be996f46465827c6d89319bf
Shivam-M <shivammann0@gmail.com>
2025-11-13 22:08:39 +0000

    update objects at a fixed rate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use eframe::egui;
+use std::time::{Duration, Instant};
 
 static WINDOW_WIDTH: f32 = 1280.0;
 static WINDOW_HEIGHT: f32 = 720.0;
+static UPDATE_RATE: Duration = Duration::from_millis(1000 / 60);
 
 fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions {
@@ -28,11 +30,16 @@ impl Object {
     fn new(width: f32, height: f32, position: egui::Pos2, velocity: egui::Vec2) -> Self {
         Self { width, height, position, velocity }
     }
+
+    fn update(&mut self) {
+        self.position += self.velocity;
+    }
 }
 
 struct CollisionApp {
     controlled_object_index: usize,  // alternative is storing the object using refcells which look complicated and messier
     objects: Vec<Object>,
+    last_update_time: Instant,
 }
 
 impl CollisionApp {
@@ -41,12 +48,12 @@ impl CollisionApp {
             50.0,
             50.0,
             egui::Pos2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0),
-            egui::Vec2::new(0.0, 0.0),
+            egui::Vec2::new(0.0, 1.0),
         );
 
         let objects = vec![controlled_object];
 
-        Self { controlled_object_index: 0, objects }
+        Self { controlled_object_index: 0, objects: objects, last_update_time: Instant::now() }
     }
 
     fn draw_object(&self, ui: &mut egui::Ui, object: &Object, centred: bool) {
@@ -66,9 +73,19 @@ impl eframe::App for CollisionApp {
             ui.heading("Hello,");
             ui.label("There should be a moveable object here soon.");
 
+            let current_time = Instant::now();
+            if current_time - self.last_update_time >= UPDATE_RATE {
+                for object in &mut self.objects {
+                    object.update();
+                }
+                self.last_update_time = current_time;
+            }
+
             for object in &self.objects {
                 self.draw_object(ui, object, true);
             }
+
+            ctx.request_repaint();
         });
     }
 }
