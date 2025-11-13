a76cdd16caca9f65240125672c8230df8cd6394d
Shivam-M <shivammann0@gmail.com>
2025-11-13 20:49:53 +0000

    create a simple window using default config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    return eframe::run_native(
+        "GUI Input Test - moving an object around with basic collision detection",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| Ok(Box::new(CollisionApp {})))
+    );
+}
+
+struct CollisionApp {}
+
+impl eframe::App for CollisionApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello,");
+            ui.label("There should be a moveable object here soon.");
+        });
+    }
 }
